e2a74aada55313a1d1557a9dfa1757c1848b4e95
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-07-13 22:50:41 +0700

    add code client use https://github.com/nguyenhuuluan434/go_math

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_math_user
+
+go 1.14
+
+require github.com/nguyenhuuluan434/go_math v0.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	gm "github.com/nguyenhuuluan434/go_math"
+)
+
+func main() {
+	add := gm.Add(1, 3)
+	fmt.Println(add)
+}
